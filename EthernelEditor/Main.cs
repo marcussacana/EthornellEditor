a78dea4ab887b8e2b64c7a46a9911a1ceb303a82
Marcus André <ddtank.marcus@gmail.com>
2016-05-09 00:14:36 -0300

    Tool Optimized
    ~not have new resounce in this release

--- EthernelEditor/Main.cs
+++ EthernelEditor/Main.cs
@@ -9,28 +9,33 @@ namespace EthornellEditor
     {
         public string[] strings = new string[0];
         public ScriptVersion Version { get; private set; }
+
+        public Encoding SJISBASE = Encoding.GetEncoding(932);
+
         private StringEntry[] Strings = new StringEntry[0];
         private int StartTable = 0;
         private byte[] script;
         private int HeaderSize = 0;
-        private object[] HeaderMask = new object[] { 0x42, 0x75, 0x72, 0x69, 0x6B, 0x6F, 0x43, 0x6F, 0x6D, 0x70, 0x69, 0x6C, 0x65, 0x64, 0x53, 0x63, 0x72, 0x69, 0x70, 0x74, 0x56, 0x65, 0x72, 0x31, 0x2E, null, null, 0x00 };
-        public void import(byte[] Script)
+
+        private object[] HeaderMask = new object[] 
+        { 0x42, 0x75, 0x72, 0x69, 0x6B, 0x6F, 0x43, 0x6F, 0x6D, 0x70, 0x69, 0x6C, 0x65, 0x64, 0x53, 0x63, 0x72, 0x69, 0x70, 0x74, 0x56, 0x65, 0x72, 0x31, 0x2E, null, null, 0x00 };
+        public void Import(byte[] Script)
         {
             script = Script;
             strings = new string[0];
             Strings = new StringEntry[0];
             StartTable = Script.Length;
-            while (!EqualAt(StartTable - 3, new byte[] { 0x00, 0x00, 0x00 }, script))
+            while (!EqualAt(StartTable - 3, new byte[] { 0x00, 0x00, 0x00 }))
                 StartTable--;
             HeaderSize = 0;
-            if (EqualAt(0, HeaderMask, Script))
+            if (EqualAt(0, HeaderMask))
             {
                 Version = ScriptVersion.WithSig;
                 HeaderSize = HeaderMask.Length + getoffset(HeaderMask.Length);
             }
             else
             {
-                if (EqualAt(0, new byte[] { 0x42, 0x53, 0x45, 0x20, 0x31, 0x2E, 0x30 }, Script))
+                if (EqualAt(0, new byte[] { 0x42, 0x53, 0x45, 0x20, 0x31, 0x2E, 0x30 }))
                 {
                     Version = ScriptVersion.BSE;
                     throw new Exception("Sorry this tool don't support the BSE encryption of the BGI\n\nIf you know how to decrypt plz add-me on skype: live:ddtank.marcus");
@@ -51,7 +56,7 @@ namespace EthornellEditor
                     finding = false;
                     Size = 0;
                 }
-                if (EqualAt(pointer, new byte[] { 0x7F, 0x00, 0x00, 0x00 }, Script) && !finding)
+                if (EqualAt(pointer, new byte[] { 0x7F, 0x00, 0x00, 0x00 }) && !finding)
                 {
                     pointer += 4;                    
                     Size = 0;
@@ -60,11 +65,11 @@ namespace EthornellEditor
                 }
                 if (finding)
                 {
-                    if (EqualAt(pointer, new byte[] { 0x03, 0x00, 0x00, 0x00 }, Script))
+                    if (EqualAt(pointer, new byte[] { 0x03, 0x00, 0x00, 0x00 }))
                     {
                     extraString:;
                         pointer += 4;
-                        int offset = Tools.ByteArrayToInt(new byte[] { Script[pointer + 3], Script[pointer + 2], Script[pointer + 1], Script[pointer] });
+                        int offset = getoffset(pointer);
                         offset += HeaderSize;
                         if (offset > Script.Length || offset < StartTable)
                         {
@@ -73,9 +78,9 @@ namespace EthornellEditor
                             continue;
                         }
                         finding = false;                        
-                        object[] rst = getString(offset, Script);
-                        string str = (string)rst[0];
-                        int length = (int)rst[1];
+                        StringEntry rst = getString(offset);
+                        string str = rst.content;
+                        int length = rst.Size;
                         object[] temp = new string[strings.Length+1];
                         strings.CopyTo(temp, 0);
                         temp[strings.Length] = str;
@@ -88,7 +93,7 @@ namespace EthornellEditor
                         SE.Size = length;
                         temp[Strings.Length] = SE;
                         Strings = (StringEntry[])temp;
-                        if (EqualAt(pointer+4, new byte[] { 0x03, 0x00, 0x00, 0x00 }, Script))//if have secondary STR
+                        if (EqualAt(pointer+4, new byte[] { 0x03, 0x00, 0x00, 0x00 }))//if have secondary STR
                         {
                             pointer += 4;
                             goto extraString;
@@ -101,7 +106,7 @@ namespace EthornellEditor
         }
         //EditorSignature = 00 Edited With EthornellEditor 00 - This need to the program know if all strings is orderned at end of the script
         private byte[] EditorSignature = new byte[] { 0x00, 0x45, 0x64, 0x69, 0x74, 0x65, 0x64, 0x20, 0x57, 0x69, 0x74, 0x68, 0x20, 0x45, 0x74, 0x68, 0x6F, 0x72, 0x6E, 0x65, 0x6C, 0x6C, 0x45, 0x64, 0x69, 0x74, 0x6F, 0x72, 0x00 };
-        public byte[] export() //maked with a prevent of strings without call order
+        public byte[] Export() //maked with a prevent of strings without call order
         {
             if (script.Length == 0)
                 throw new Exception("You need import before export.");
@@ -128,7 +133,7 @@ namespace EthornellEditor
                 //step 3.1 - Detect Start Of StringTable
                 for (int pos = script.Length - EditorSignature.Length - HeaderMask.Length; pos > 0; pos--)
                 {
-                    if (EqualAt(pos, EditorSignature, script))
+                    if (EqualAt(pos, EditorSignature))
                     {
                         TableStart = pos;
                         break;
@@ -161,15 +166,7 @@ namespace EthornellEditor
             {
                 int Offset = (TableStart + EditorSignature.Length - 1) + StringTable.Length;
                 int OffsetPos = Strings[pos].OffsetPos;
-                string[] hexs = Tools.SJStringToHex(strings[pos].Replace("\\n", "\n"));
-                string hex = "";
-                for (int i = 0; i < hexs.Length; i++)
-                {
-                    hex += hexs[i] + " ";
-                }
-                hex = hex.Substring(0, hex.Length-1);
-                byte[] newstring = Tools.StringToByteArray(hex);
-                newstring = insertArr(newstring, new byte[] { 0x00 });
+                byte[] newstring = SJISBASE.GetBytes(strings[pos].Replace("\\n", "\n") + "\x0");
                 StringTable = insertArr(StringTable, newstring);
                 int[] OffPos = (int[])offsets[0];
                 int[] Offsets = (int[])offsets[1];
@@ -243,29 +240,10 @@ namespace EthornellEditor
 
         private byte[] genOffet(int offset)
         {
-            byte[] result = new byte[4];
-            string hex = Tools.IntToHex(offset);
-            if (hex.Length % 2 != 0)
-            {
-                hex = "0" + hex;
-            }
-            byte[] off = Tools.StringToByteArray(hex);
-            switch (off.Length)
-            {
-                case 1:
-                    result = new byte[] { off[0], 0x00, 0x00, 0x00 };
-                    break;
-                case 2:
-                    result = new byte[] { off[1], off[0], 0x00, 0x00 };
-                    break;
-                case 3:
-                    result = new byte[] { off[2], off[1], off[0], 0x00 };
-                    break;
-                case 4:
-                    result = new byte[] { off[3], off[2], off[1], off[0] };
-                    break;
-            }
-            return result;
+            byte[] Off = BitConverter.GetBytes(offset);
+            if (!BitConverter.IsLittleEndian)
+                Array.Reverse(Off);
+            return Off;
 
         }
 
@@ -296,32 +274,41 @@ namespace EthornellEditor
         }
 
         private int getoffset(int pos) {
-            return Tools.ByteArrayToInt(new byte[] { script[pos+3], script[pos+2], script[pos+1], script[pos] });
+            byte[] off = new byte[4];
+            off[0] = script[pos]; off[1] = script[pos + 1]; off[2] = script[pos + 2]; off[3] = script[pos + 3];
+            if (!BitConverter.IsLittleEndian)
+                Array.Reverse(off);
+            return BitConverter.ToInt32(off, 0);
         }
 
-        private object[] getString(int offset, byte[] script)
+        private StringEntry getString(int offset)
         {
-            byte[] str = new byte[0];
-            while (script[offset] != 0x00)
+            int ps = offset;
+            while (script[ps] != 0x00)
+                ps++;
+            int len = ps - offset;
+            ps = 0;
+            byte[] str = new byte[len];
+            while (ps < len)
             {
-                byte[] temp = new byte[str.Length + 1];
-                str.CopyTo(temp, 0);
-                temp[str.Length] = script[offset];
-                str = temp;
-                offset++;
+                str[ps] = script[offset + ps];
+                ps++;
             }
-            string[] hex = Tools.ByteArrayToString(str).Split('-');
-            return new object[] { Tools.SJHexToString(hex).Replace("\n", "\\n"), hex.Length };
+            return new StringEntry() {
+                OffsetPos = offset,
+                Size = len,
+                content = SJISBASE.GetString(str)
+            };
         }
 
-        private bool EqualAt(int offset, byte[] check, byte[] script)
+        private bool EqualAt(int offset, byte[] check)
         {
             for (int index = 0; index < check.Length; index++)
                 if (check[index] != script[index + offset])
                     return false;
             return true;
         }
-        private bool EqualAt(int offset, object[] check, byte[] script)
+        private bool EqualAt(int offset, object[] check)
         {
             for (int index = 0; index < check.Length; index++)
                 if (check[index] is byte || check[index] is int)
@@ -339,153 +326,5 @@ namespace EthornellEditor
         public int OffsetPos = 0;
         public int Size = 0;
         public string content = "";
-    }
-    class Tools
-    {
-        public static string IntToHex(int val)
-        {
-            return val.ToString("X");
-        }
-        public static int ByteArrayToInt(byte[] array)
-        {
-            return Tools.HexToInt(Tools.ByteArrayToString(array).Replace(@"-", ""));
-        }
-        public static string StringToHex(string _in)
-        {
-            string input = _in;
-            char[] values = input.ToCharArray();
-            string r = "";
-            foreach (char letter in values)
-            {
-                int value = Convert.ToInt32(letter);
-                string hexOutput = System.String.Format("{0:X}", value);
-                if (value > 255)
-                    return UnicodeStringToHex(input);
-                r += value + " ";
-            }
-            string[] bytes = r.Split(' ');
-            byte[] b = new byte[bytes.Length - 1];
-            int index = 0;
-            foreach (string val in bytes)
-            {
-                if (index == bytes.Length - 1)
-                    break;
-                if (int.Parse(val) > byte.MaxValue)
-                {
-                    b[index] = byte.Parse("0");
-                }
-                else
-                    b[index] = byte.Parse(val);
-                index++;
-            }
-            r = ByteArrayToString(b);
-            return r.Replace("-", @" ");
-        }
-        public static string UnicodeStringToHex(string _in)
-        {
-            string input = _in;
-            char[] values = Encoding.Unicode.GetChars(Encoding.Unicode.GetBytes(input.ToCharArray()));
-            string r = "";
-            foreach (char letter in values)
-            {
-                int value = Convert.ToInt32(letter);
-                string hexOutput = System.String.Format("{0:X}", value);
-                r += value + " ";
-            }
-            UnicodeEncoding unicode = new UnicodeEncoding();
-            byte[] b = unicode.GetBytes(input);
-            r = ByteArrayToString(b);
-            return r.Replace("-", @" ");
-
-        }
-        public static string U8HexToString(string[] hex)
-        {
-            byte[] str = StringToByteArray(hex);
-            UTF8Encoding encoder = new UTF8Encoding();
-            return encoder.GetString(str);
-        }
-        public static string[] U8StringToHex(string text)
-        {
-            UTF8Encoding encoder = new UTF8Encoding();
-            byte[] cnt = encoder.GetBytes(text.ToCharArray());
-            return ByteArrayToString(cnt).Split('-');
-        }
-
-        public static string SJHexToString(string[] hex)
-        {
-            byte[] str = StringToByteArray(hex);
-            Encoding encoder = Encoding.GetEncoding(932);
-            return encoder.GetString(str);
-        }
-        public static string[] SJStringToHex(string text)
-        {
-            Encoding encoder = Encoding.GetEncoding(932);
-            byte[] cnt = encoder.GetBytes(text.ToCharArray());
-            return ByteArrayToString(cnt).Split('-');
-        }
-        public static byte[] StringToByteArray(string hex)
-        {
-            try
-            {
-                hex = hex.Replace(@" ", "");
-                int NumberChars = hex.Length;
-                byte[] bytes = new byte[NumberChars / 2];
-                for (int i = 0; i < NumberChars; i += 2)
-                    bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
-                return bytes;
-            }
-            catch { Console.Write("Invalid format file!"); return new byte[0]; }
-        }
-        public static byte[] StringToByteArray(string[] hex)
-        {
-            try
-            {
-                int NumberChars = hex.Length;
-                byte[] bytes = new byte[NumberChars];
-                for (int i = 0; i < NumberChars; i++)
-                    bytes[i] = Convert.ToByte(hex[i], 16);
-                return bytes;
-            }
-            catch { Console.Write("Invalid format file!"); return new byte[0]; }
-        }
-        public static string ByteArrayToString(byte[] ba)
-        {
-            string hex = BitConverter.ToString(ba);
-            return hex;
-        }
-
-        public static int HexToInt(string hex)
-        {
-            int num = Int32.Parse(hex, System.Globalization.NumberStyles.HexNumber);
-            return num;
-        }
-
-        public static string HexToString(string hex)
-        {
-            string[] hexValuesSplit = hex.Split(' ');
-            string returnvar = "";
-            foreach (string hexs in hexValuesSplit)
-            {
-                int value = Convert.ToInt32(hexs, 16);
-                char charValue = (char)value;
-                returnvar += charValue;
-            }
-            return returnvar;
-        }
-
-        public static string UnicodeHexToUnicodeString(string hex)
-        {
-            string hexString = hex.Replace(@" ", "");
-            int length = hexString.Length;
-            byte[] bytes = new byte[length / 2];
-
-            for (int i = 0; i < length; i += 2)
-            {
-                bytes[i / 2] = Convert.ToByte(hexString.Substring(i, 2), 16);
-            }
-
-            return Encoding.Unicode.GetString(bytes);
-        }
-
     }
 }
